9281d7e97faa1ee2f72765eeb41f77805e91ebeb
yunusemreozdemir <emreozdemir@mail.com>
2025-05-15 20:27:11 +0300

    feat: add terraform and kubernetes

---
+++ terraform/main.tf
@@ -0,0 +1,42 @@
+provider "google" {
+  project = var.project_id
+  region  = var.region
+}
+
+resource "google_artifact_registry_repository" "fastapi_repo" {
+  provider = google
+  location = var.region
+  repository_id = var.repo_name
+  description   = "Artifact Registry for FastAPI App"
+  format        = "DOCKER"
+}
+
+resource "google_container_cluster" "primary" {
+  name     = "fastapi-cluster"
+  location = var.region
+
+  remove_default_node_pool = true
+  initial_node_count       = 1
+
+  node_config {
+    machine_type = "e2-medium"
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/cloud-platform"
+    ]
+  }
+}
+
+resource "google_container_node_pool" "primary_nodes" {
+  name       = "default-node-pool"
+  cluster    = google_container_cluster.primary.name
+  location   = var.region
+  node_count = 1
+
+  node_config {
+    preemptible  = false
+    machine_type = "e2-medium"
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/cloud-platform"
+    ]
+  }
+}
---
+++ terraform/variables.tf
@@ -0,0 +1,7 @@
+variable "project_id" {}
+variable "region" {
+  default = "europe-west1"
+}
+variable "repo_name" {
+  default = "devsecops-images"
+}
